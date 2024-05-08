842b8030a0aa4fc535ef552dc848f2645f2b30a4
telontelvarost <telontelvarost@gmail.com>
2024-05-07 22:01:26 -0400

    UDOC-0 - Update to latest StationAPI alpha.2 and migrate to biny v2 mappings; Use more compatible mixin for disabling cursor

--- src/main/java/com/github/telvarost/hudtweaks/mixin/InGameMixin.java
+++ src/main/java/com/github/telvarost/hudtweaks/mixin/InGameMixin.java
@@ -2,13 +2,15 @@ package com.github.telvarost.hudtweaks.mixin;
 
 import com.github.telvarost.hudtweaks.Config;
 import com.github.telvarost.hudtweaks.CoordinateDisplayEnum;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.DrawableHelper;
-import net.minecraft.client.gui.InGame;
-import net.minecraft.client.gui.screen.ingame.Chat;
-import net.minecraft.client.render.TextRenderer;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.gui.screen.ChatScreen;
 import org.lwjgl.input.Mouse;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,100 +21,100 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.List;
 import java.util.Random;
 
-@Mixin(InGame.class)
+@Mixin(InGameHud.class)
 @Environment(value= EnvType.CLIENT)
-public abstract class InGameMixin extends DrawableHelper {
+public abstract class InGameMixin extends DrawContext {
 
     @Shadow private Minecraft minecraft;
 
-    @Shadow private List chatMessages;
+    @Shadow private List messages;
 
-    @Shadow private Random rand;
+    @Shadow private Random random;
     @Unique private Integer numberOfTurns = 0;
 
     @Unique private Integer chatOffset = 0;
 
     @Redirect(
-            method = "renderHud",
+            method = "render",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/InGame;blit(IIIIII)V",
+                    target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(IIIIII)V",
                     ordinal = 0
             )
     )
-    private void hudTweaks_renderHudHotbarPosition(InGame instance, int i, int j, int k, int l, int m, int n) {
-        instance.blit(i, j - Config.config.hotbarYPositionOffset, k, l, m, n);
+    private void hudTweaks_renderHotbarPosition(InGameHud instance, int i, int j, int k, int l, int m, int n) {
+        instance.drawTexture(i, j - Config.config.hotbarYPositionOffset, k, l, m, n);
     }
 
     @Redirect(
-            method = "renderHud",
+            method = "render",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/InGame;blit(IIIIII)V",
+                    target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(IIIIII)V",
                     ordinal = 1
             )
     )
-    private void hudTweaks_renderHudSelectedItemBorderPosition(InGame instance, int i, int j, int k, int l, int m, int n) {
-        instance.blit(i, j - Config.config.hotbarYPositionOffset, k, l, m, n);
+    private void hudTweaks_renderSelectedItemBorderPosition(InGameHud instance, int i, int j, int k, int l, int m, int n) {
+        instance.drawTexture(i, j - Config.config.hotbarYPositionOffset, k, l, m, n);
     }
 
     @ModifyConstant(
-            method = "renderHud",
+            method = "render",
             constant = @Constant(intValue = 32, ordinal = 0)
     )
-    private int hudTweaks_renderHudStatusBarPositions0(int value) {
+    private int hudTweaks_renderStatusBarPositions0(int value) {
         return value + Config.config.hotbarYPositionOffset;
     }
 
     @ModifyConstant(
-            method = "renderHud",
+            method = "render",
             constant = @Constant(intValue = 32, ordinal = 1)
     )
-    private int hudTweaks_renderHudStatusBarPositions1(int value) {
+    private int hudTweaks_renderStatusBarPositions1(int value) {
         return value + Config.config.hotbarYPositionOffset;
     }
 
     @ModifyConstant(
-            method = "renderHud",
+            method = "render",
             constant = @Constant(intValue = 32, ordinal = 2)
     )
-    private int hudTweaks_renderHudStatusBarPositions2(int value) {
+    private int hudTweaks_renderStatusBarPositions2(int value) {
         return value + Config.config.hotbarYPositionOffset;
     }
 
     @ModifyConstant(
-            method = "renderHud",
+            method = "render",
             constant = @Constant(intValue = 16, ordinal = 5)
     )
-    private int hudTweaks_renderHudItemPositions(int value) {
+    private int hudTweaks_renderItemPositions(int value) {
         return value + Config.config.hotbarYPositionOffset;
     }
 
     @ModifyConstant(
-            method = "renderHud",
+            method = "render",
             constant = @Constant(intValue = 200)
     )
-    private int hudTweaks_renderHudChatFadeTime(int value) {
+    private int hudTweaks_renderChatFadeTime(int value) {
         return (Config.config.chatFadeTime * 2);
     }
 
     @ModifyConstant(
-            method = "renderHud",
+            method = "render",
             constant = @Constant(doubleValue = 200.0)
     )
-    private double hudTweaks_renderHudChatFadeTimeDivisor(double value) {
+    private double hudTweaks_renderChatFadeTimeDivisor(double value) {
         return (Config.config.chatFadeTime * 2);
     }
 
-    @Inject(method = "renderHud", at = @At("HEAD"), cancellable = true)
-    public void hudTweaks_renderHudChatScroll(float f, boolean bl, int i, int j, CallbackInfo ci) {
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    public void hudTweaks_renderChatScroll(float f, boolean bl, int i, int j, CallbackInfo ci) {
         if (!Config.config.enableChatScroll) {
             return;
         }
 
         int chatRangeTop = 20;
 
-        if (this.minecraft.currentScreen instanceof Chat) {
+        if (this.minecraft.currentScreen instanceof ChatScreen) {
             int currentWheelDegrees = Mouse.getDWheel();
             numberOfTurns = Math.round((float)currentWheelDegrees / 120.0f);
             chatOffset = chatOffset + numberOfTurns;
@@ -121,9 +123,9 @@ public abstract class InGameMixin extends DrawableHelper {
                 chatOffset = 0;
             }
 
-            if (chatRangeTop < this.chatMessages.size()) {
-                if (this.chatMessages.size() <= (chatRangeTop + chatOffset)) {
-                    chatOffset = this.chatMessages.size() - chatRangeTop;
+            if (chatRangeTop < this.messages.size()) {
+                if (this.messages.size() <= (chatRangeTop + chatOffset)) {
+                    chatOffset = this.messages.size() - chatRangeTop;
                 }
             } else {
                 chatOffset = 0;
@@ -133,85 +135,87 @@ public abstract class InGameMixin extends DrawableHelper {
         }
     }
 
-    @Redirect(
-            method = "renderHud",
+    @WrapOperation(
+            method = "render",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/InGame;blit(IIIIII)V",
+                    target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(IIIIII)V",
                     ordinal = 2
             )
     )
-    public void hudTweaks_renderCursor(InGame instance, int i, int j, int k, int l, int m, int n) {
+    private void hudTweaks_renderCursor(InGameHud instance, int i, int j, int k, int l, int m, int n, Operation<Void> original) {
         if (!Config.config.disableCrosshair) {
-            instance.blit(i, j, k, l, m, n);
+            original.call(instance, i, j, k, l, m, n);
+        } else {
+            original.call(instance, 0, 0, 0, 0, 0, 0);
         }
     }
 
     @Redirect(
-            method = "renderHud",
+            method = "render",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/InGame;drawTextWithShadow(Lnet/minecraft/client/render/TextRenderer;Ljava/lang/String;III)V",
+                    target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V",
                     ordinal = 2
             )
     )
-    public void hudTweaks_renderDrawCoordinate_X(InGame instance, TextRenderer textRenderer, String s, int i, int j, int k) {
+    public void hudTweaks_renderDrawCoordinate_X(InGameHud instance, TextRenderer textRenderer, String s, int i, int j, int k) {
         if (CoordinateDisplayEnum.HIDE == Config.config.coordinateDisplay) {
             instance.drawTextWithShadow(textRenderer, "", i, j, k);
         } else if (CoordinateDisplayEnum.RANDOMIZE == Config.config.coordinateDisplay) {
-            double randVal = rand.nextDouble(-10000, 10000);
-            instance.drawTextWithShadow(textRenderer, "x: " + randVal, i, j, k);
+            double randomVal = random.nextDouble(-10000, 10000);
+            instance.drawTextWithShadow(textRenderer, "x: " + randomVal, i, j, k);
         } else {
             instance.drawTextWithShadow(textRenderer, s, i, j, k);
         }
     }
 
     @Redirect(
-            method = "renderHud",
+            method = "render",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/InGame;drawTextWithShadow(Lnet/minecraft/client/render/TextRenderer;Ljava/lang/String;III)V",
+                    target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V",
                     ordinal = 3
             )
     )
-    public void hudTweaks_renderDrawCoordinate_Y(InGame instance, TextRenderer textRenderer, String s, int i, int j, int k) {
+    public void hudTweaks_renderDrawCoordinate_Y(InGameHud instance, TextRenderer textRenderer, String s, int i, int j, int k) {
         if (CoordinateDisplayEnum.HIDE == Config.config.coordinateDisplay) {
             instance.drawTextWithShadow(textRenderer, "", i, j, k);
         } else if (CoordinateDisplayEnum.RANDOMIZE == Config.config.coordinateDisplay) {
-            double randVal = rand.nextDouble(-10000, 10000);
-            instance.drawTextWithShadow(textRenderer, "y: " + randVal, i, j, k);
+            double randomVal = random.nextDouble(-10000, 10000);
+            instance.drawTextWithShadow(textRenderer, "y: " + randomVal, i, j, k);
         } else {
             instance.drawTextWithShadow(textRenderer, s, i, j, k);
         }
     }
 
     @Redirect(
-            method = "renderHud",
+            method = "render",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/InGame;drawTextWithShadow(Lnet/minecraft/client/render/TextRenderer;Ljava/lang/String;III)V",
+                    target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V",
                     ordinal = 4
             )
     )
-    public void hudTweaks_renderDrawCoordinate_Z(InGame instance, TextRenderer textRenderer, String s, int i, int j, int k) {
+    public void hudTweaks_renderDrawCoordinate_Z(InGameHud instance, TextRenderer textRenderer, String s, int i, int j, int k) {
         if (CoordinateDisplayEnum.HIDE == Config.config.coordinateDisplay) {
             instance.drawTextWithShadow(textRenderer, "", i, j, k);
         } else if (CoordinateDisplayEnum.RANDOMIZE == Config.config.coordinateDisplay) {
-            double randVal = rand.nextDouble(-10000, 10000);
-            instance.drawTextWithShadow(textRenderer, "z: " + randVal, i, j, k);
+            double randomVal = random.nextDouble(-10000, 10000);
+            instance.drawTextWithShadow(textRenderer, "z: " + randomVal, i, j, k);
         } else {
             instance.drawTextWithShadow(textRenderer, s, i, j, k);
         }
     }
 
     @Redirect(
-            method = "renderHud",
+            method = "render",
             at = @At(
                     value = "INVOKE",
                     target = "Ljava/util/List;get(I)Ljava/lang/Object;"
             )
     )
-    public Object hudTweaks_renderHudChatOffset(List instance, int i) {
+    public Object hudTweaks_renderChatOffset(List instance, int i) {
         if (Config.config.enableChatScroll) {
             return instance.get(i + chatOffset);
         } else {
