f02b570a77e4622e23742e2864328b00df336194
telvarost <telontelvarost@gmail.com>
2024-10-28 20:57:12 -0400

    Migrate to GCAPI3

--- src/main/java/com/github/telvarost/hudtweaks/Config.java
+++ src/main/java/com/github/telvarost/hudtweaks/Config.java
@@ -1,55 +1,60 @@
 package com.github.telvarost.hudtweaks;
 
-import blue.endless.jankson.Comment;
-import net.glasslauncher.mods.api.gcapi.api.ConfigName;
-import net.glasslauncher.mods.api.gcapi.api.GConfig;
-import net.glasslauncher.mods.api.gcapi.api.MaxLength;
+import net.glasslauncher.mods.gcapi3.api.*;
 
 public class Config {
 
-    @GConfig(value = "config", visibleName = "HUD Tweaks")
+    @ConfigRoot(value = "config", visibleName = "HUD Tweaks")
     public static ConfigFields config = new ConfigFields();
 
     public static class ConfigFields {
-        @ConfigName("Allow Chat Scroll")
+        @ConfigEntry(name = "Allow Chat Scroll")
         public Boolean enableChatScroll = true;
 
-        @ConfigName("Chat History Size")
-        @MaxLength(4096)
-        @Comment("50 = vanilla, 100 = default")
+        @ConfigEntry(
+                name = "Chat History Size",
+                description = "50 = vanilla, 100 = default",
+                maxLength = 4096
+        )
         public Integer chatHistorySize = 100;
 
-        @ConfigName("Chat Message Fade Time")
-        @MaxLength(32000)
-        @Comment("100 = vanilla, 10 = 1 second")
+        @ConfigEntry(
+                name = "Chat Message Fade Time",
+                description = "100 = vanilla, 10 = 1 second",
+                maxLength = 32000
+        )
         public Integer chatFadeTime = 100;
 
-        @ConfigName("Coordinate Display For Debug Overlay")
+        @ConfigEntry(name = "Coordinate Display For Debug Overlay")
         public CoordinateDisplayEnum coordinateDisplay = CoordinateDisplayEnum.SHOW;
 
-        @ConfigName("Disable Crosshair")
+        @ConfigEntry(name = "Disable Crosshair")
         public Boolean disableCrosshair = false;
 
-        @ConfigName("Draw Xbox X And Y Buttons")
+        @ConfigEntry(name = "Draw Xbox X And Y Buttons")
         public Boolean drawXboxXAndYButtons = false;
 
-        @ConfigName("Enable Hotbar Item Selection Tooltips")
+        @ConfigEntry(name = "Enable Hotbar Item Selection Tooltips")
         public Boolean enableHotbarItemSelectionTooltips = false;
 
-        @ConfigName("Hotbar Item Selection Fade Time")
-        @MaxLength(32000)
-        @Comment("40 = default")
+        @ConfigEntry(
+                name = "Hotbar Item Selection Fade Time",
+                description = "40 = default",
+                maxLength = 32000
+        )
         public Integer hotbarItemSelectionFadeTime = 40;
 
-        @ConfigName("Hotbar Position")
-        @MaxLength(32000)
-        @Comment("0 = vanilla, 32 = xbox, 218 = top")
+        @ConfigEntry(
+                name = "Hotbar Position",
+                description = "0 = vanilla, 32 = xbox, 218 = top",
+                maxLength = 32000
+        )
         public Integer hotbarYPositionOffset = 0;
 
-        @ConfigName("Put Item Selection Tooltip Below Hotbar")
+        @ConfigEntry(name = "Put Item Selection Tooltip Below Hotbar")
         public Boolean putItemSelectionTooltipBelowHotbar = false;
 
-        @ConfigName("Put Status Bar Icons Below Hotbar")
+        @ConfigEntry(name = "Put Status Bar Icons Below Hotbar")
         public Boolean putStatusBarIconsBelowHotbar = false;
     }
 }
--- src/main/java/com/github/telvarost/hudtweaks/CoordinateDisplayEnumFactory.java
+++ src/main/java/com/github/telvarost/hudtweaks/CoordinateDisplayEnumFactory.java
@@ -1,22 +1,19 @@
 package com.github.telvarost.hudtweaks;
 
-import blue.endless.jankson.JsonElement;
-import blue.endless.jankson.JsonPrimitive;
 import com.google.common.collect.ImmutableMap;
-import net.glasslauncher.mods.api.gcapi.api.ConfigFactoryProvider;
-import net.glasslauncher.mods.api.gcapi.api.MaxLength;
-import net.glasslauncher.mods.api.gcapi.impl.NonFunction;
-import net.glasslauncher.mods.api.gcapi.impl.config.ConfigEntry;
-import net.glasslauncher.mods.api.gcapi.impl.config.entry.EnumConfigEntry;
+import net.glasslauncher.mods.gcapi3.api.*;
+import net.glasslauncher.mods.gcapi3.impl.SeptFunction;
+import net.glasslauncher.mods.gcapi3.impl.object.ConfigEntryHandler;
+import net.glasslauncher.mods.gcapi3.impl.object.entry.EnumConfigEntryHandler;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Type;
-import java.util.function.Function;
+import java.lang.reflect.*;
+import java.util.function.*;
 
 public class CoordinateDisplayEnumFactory implements ConfigFactoryProvider {
+
     @Override
-    public void provideLoadFactories(ImmutableMap.Builder<Type, NonFunction<String, String, String, Field, Object, Boolean, Object, Object, MaxLength, ConfigEntry<?>>> immutableBuilder) {
-        immutableBuilder.put(CoordinateDisplayEnum.class, ((id, name, description, parentField, parentObject, isMultiplayerSynced, enumOrOrdinal, defaultEnum, maxLength) ->
+    public void provideLoadFactories(ImmutableMap.Builder<Type, SeptFunction<String, ConfigEntry, Field, Object, Boolean, Object, Object, ConfigEntryHandler<?>>> immutableBuilder) {
+        immutableBuilder.put(CoordinateDisplayEnum.class, ((id, configEntry, parentField, parentObject, isMultiplayerSynced, enumOrOrdinal, defaultEnum) ->
         {
             int enumOrdinal;
             if(enumOrOrdinal instanceof Integer ordinal) {
@@ -25,17 +22,12 @@ public class CoordinateDisplayEnumFactory implements ConfigFactoryProvider {
             else {
                 enumOrdinal = ((CoordinateDisplayEnum) enumOrOrdinal).ordinal();
             }
-            return new EnumConfigEntry<CoordinateDisplayEnum>(id, name, description, parentField, parentObject, isMultiplayerSynced, enumOrdinal, ((CoordinateDisplayEnum) defaultEnum).ordinal(), CoordinateDisplayEnum.class);
+            return new EnumConfigEntryHandler<CoordinateDisplayEnum>(id, configEntry, parentField, parentObject, isMultiplayerSynced, enumOrdinal, ((CoordinateDisplayEnum) defaultEnum).ordinal(), CoordinateDisplayEnum.class);
         }));
     }
 
     @Override
-    public void provideSaveFactories(ImmutableMap.Builder<Type, Function<Object, JsonElement>> immutableBuilder) {
-        immutableBuilder.put(CoordinateDisplayEnum.class, enumEntry -> new JsonPrimitive(((CoordinateDisplayEnum) enumEntry).ordinal()));
-    }
-
-    @Override
-    public void provideLoadTypeAdapterFactories(@SuppressWarnings("rawtypes") ImmutableMap.Builder<Type, Class> immutableBuilder) {
-        immutableBuilder.put(CoordinateDisplayEnum.class, Integer.class);
+    public void provideSaveFactories(ImmutableMap.Builder<Type, Function<Object, Object>> immutableBuilder) {
+        immutableBuilder.put(CoordinateDisplayEnum.class, enumEntry -> enumEntry);
     }
 }
